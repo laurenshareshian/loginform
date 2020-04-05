98d3cc78e81f38f1e054873f38e9313baff82841
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2020-04-04 20:33:20 -0700

    created member and role repositories

---
+++ src/main/java/web/backtospring/repositories/MemberRepository.java
@@ -0,0 +1,8 @@
+package web.backtospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.backtospring.entities.Member;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+}
