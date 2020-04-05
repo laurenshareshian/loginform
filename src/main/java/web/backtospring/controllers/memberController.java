fbb33e158accac0d54c94168a08d030fa44abd72
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2020-04-04 20:45:48 -0700

    created get and post request for register module

--- src/main/java/web/backtospring/controllers/memberController.java
+++ src/main/java/web/backtospring/controllers/memberController.java
@@ -1,19 +1,29 @@
 package web.backtospring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import web.backtospring.entities.Member;
+import web.backtospring.services.MemberService;
+
+import javax.validation.Valid;
 
 @Controller
 public class memberController {
 
-    @GetMapping("/login")
-    public String showLoginForm(){
-        return "views/loginForm";
-    }
+    @Autowired
+    MemberService memberService;
 
     @GetMapping("/register")
     public String registerForm(Model model){
+        model.addAttribute("member", new Member());
         return "views/registerForm";
+    }
+    @PostMapping("/register")
+    public String registerMember(@Valid Member member){
+        memberService.createMember(member);
+        return "views/loginForm";
     }
 }
