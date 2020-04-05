1fbc809bdc66ea6e62c1b8bcc2d075e6c89a3316
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2020-04-04 17:33:31 -0700

    created Thymeleaf attributes on html pages

--- src/main/java/web/backtospring/controllers/IndexController.java
+++ src/main/java/web/backtospring/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package web.backtospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String showIndex(){
+    public String showIndex(Model model){
+        model.addAttribute("message", "Hello everyone, we are go to back to Spring with together");
+        model.addAttribute("date", new Date());
         return "index";
     }
 }
